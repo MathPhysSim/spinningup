967e4ebf8b13d117ddea7a6fba18f1d474e47da0
Simon Hirlander <simon.hirlaender@cern.ch>
2019-08-26 14:41:36 +0200

    - first debugged NAF version

--- plot_results.py
+++ plot_results.py
@@ -1,3 +1,3 @@
 import spinup.utils.plot as plot_tools
 
-plot_tools.make_plots(['spinup/logging/NAF/naf-bench-long_pendulum-v0'])
+plot_tools.make_plots(['/Users/shirlaen/PycharmProjects/DeepLearning/spinningup/spinup/logging/NAF/naf-bench-long_pendulum-v0_hid100-100'])
